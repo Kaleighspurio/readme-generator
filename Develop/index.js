1918f68f98ddbc801c2de7a4f41d13a750b32fe5
Kaleigh Spurio <kaleighastell@gmail.com>
2020-05-25 11:12:44 -0400

    completed npm init and installed inquirer and dotenv

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,33 @@
 const questions = [
+/*   
+This will be an array of questions.
 
+The README will be populated with the following:
+
+At least one badge
+What is the Project title?
+What is the Description?
+What is the Table of Contents? [Array or an object]
+How to Install?
+What is the Usage?
+What is the License?
+Who are the Contributors? [array of contributors]
+What are your Tests?
+Questions - see below
+    What is your User GitHub profile picture? (image url?)
+    What is your User GitHub email?
+*/
 ];
 
 function writeToFile(fileName, data) {
+    // where do I want the file placed? do I need to check?  use a relative path?
+    // create a file named fileName
+    // write to file fileName the data
+
 }
 
 function init() {
-
+    // initialize stuff that I need here
 }
 
 init();
